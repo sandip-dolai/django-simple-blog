1fae34c21b65347b1cc84267f692181ea0c83480
Sandip Dolai <sd694722@gmail.com>
2025-07-12 22:13:12 +0530

    Make some modifications

--- myblog/models.py
+++ myblog/models.py
@@ -1,7 +1,8 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db import models # type: ignore
+from django.contrib.auth.models import User # type: ignore
 
 class Post(models.Model):
+    title_tag = models.CharField(max_length=100, default='My Blog')
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     body = models.TextField()
